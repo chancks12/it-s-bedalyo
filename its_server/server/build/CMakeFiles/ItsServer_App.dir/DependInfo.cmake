bd44ac8792a6dd4759c831f69e8e58cdf792d001
chan <hohoit0512@gmail.com>
2026-04-01 16:57:31 +0900

    first commit

---
+++ its_server/server/build/CMakeFiles/ItsServer_App.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lms/Documents/its_server/server/main.cpp" "CMakeFiles/ItsServer_App.dir/main.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/main.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/AddressDAO.cpp" "CMakeFiles/ItsServer_App.dir/src/database/AddressDAO.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/AddressDAO.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/AdminDAO.cpp" "CMakeFiles/ItsServer_App.dir/src/database/AdminDAO.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/AdminDAO.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/AuthDAO.cpp" "CMakeFiles/ItsServer_App.dir/src/database/AuthDAO.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/AuthDAO.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/CategoryDAO.cpp" "CMakeFiles/ItsServer_App.dir/src/database/CategoryDAO.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/CategoryDAO.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/DbManager.cpp" "CMakeFiles/ItsServer_App.dir/src/database/DbManager.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/DbManager.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/MembershipDAO.cpp" "CMakeFiles/ItsServer_App.dir/src/database/MembershipDAO.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/MembershipDAO.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/MenuDAO.cpp" "CMakeFiles/ItsServer_App.dir/src/database/MenuDAO.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/MenuDAO.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/OrderDAO.cpp" "CMakeFiles/ItsServer_App.dir/src/database/OrderDAO.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/OrderDAO.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/SearchDAO.cpp" "CMakeFiles/ItsServer_App.dir/src/database/SearchDAO.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/SearchDAO.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/StoreDAO.cpp" "CMakeFiles/ItsServer_App.dir/src/database/StoreDAO.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/StoreDAO.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/database/UserDAO.cpp" "CMakeFiles/ItsServer_App.dir/src/database/UserDAO.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/database/UserDAO.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/AddressHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/AddressHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/AddressHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/AdminHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/AdminHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/AdminHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/CategoryHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/CategoryHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/CategoryHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/ChatHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/ChatHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/ChatHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/Dispatcher.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/Dispatcher.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/Dispatcher.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/MenuHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/MenuHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/MenuHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/OrderHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/OrderHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/OrderHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/ReviewHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/ReviewHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/ReviewHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/RiderHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/RiderHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/RiderHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/SalesHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/SalesHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/SalesHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/SearchHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/SearchHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/SearchHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/StoreHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/StoreHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/StoreHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/SystemHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/SystemHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/SystemHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/handler/UserHandler.cpp" "CMakeFiles/ItsServer_App.dir/src/handler/UserHandler.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/handler/UserHandler.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/network/ItsServer.cpp" "CMakeFiles/ItsServer_App.dir/src/network/ItsServer.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/network/ItsServer.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/network/PacketFramer.cpp" "CMakeFiles/ItsServer_App.dir/src/network/PacketFramer.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/network/PacketFramer.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/service/OrderService.cpp" "CMakeFiles/ItsServer_App.dir/src/service/OrderService.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/service/OrderService.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/session/ChatRoomManager.cpp" "CMakeFiles/ItsServer_App.dir/src/session/ChatRoomManager.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/session/ChatRoomManager.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/session/ClientSession.cpp" "CMakeFiles/ItsServer_App.dir/src/session/ClientSession.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/session/ClientSession.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/session/SessionManager.cpp" "CMakeFiles/ItsServer_App.dir/src/session/SessionManager.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/session/SessionManager.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/thread/ThreadPool.cpp" "CMakeFiles/ItsServer_App.dir/src/thread/ThreadPool.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/thread/ThreadPool.cpp.o.d"
+  "/home/lms/Documents/its_server/server/src/util/TimeUtil.cpp" "CMakeFiles/ItsServer_App.dir/src/util/TimeUtil.cpp.o" "gcc" "CMakeFiles/ItsServer_App.dir/src/util/TimeUtil.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
